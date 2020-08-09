0a3373cfc26c7d40089e6ebd1a92553e768a2e4e
Tegar Wisnu Pambudi <sukandaru.japan@gmail.com>
2020-08-09 07:37:33 +0000

    Canary

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/nangjogja/public_html/nangjogja/
+
+echo "+++++++++++++++++++++++++++++++++++++++ Create Proxy and Internal Network ++++++++++++++++++++++++++++++++++++++"
+	docker network create proxy && docker network create internal
+
+echo "+++++++++++++++++++++++++++++++++++++++ Build App ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	docker-compose up --no-deps -d --build --remove-orphans
+	docker-compose ps
+
+echo "+++++++++++++++++++++++++++++++++++++++ App Ready, Wait for supervisord ++++++++++++++++++++++++++++++++++++++++"
